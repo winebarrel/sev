602ce32b8f7712eddb9e219b2defd842032bfdb0
winebarrel <sugawara@winebarrel.jp>
2024-07-05 13:04:46 +0900

    Add test

---
+++ exec_cmd_test.go
@@ -0,0 +1,89 @@
+package sev
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_execCmd_OK(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	cmd := []string{"/usr/bin/env"}
+	env := map[string]string{
+		"FOO": "BAR",
+		"ZOO": "BAZ",
+	}
+
+	bufout := &bytes.Buffer{}
+	buferr := &bytes.Buffer{}
+	_stdout = bufout
+	_stderr = buferr
+
+	defer func() {
+		_stdout = os.Stdout
+		_stderr = os.Stderr
+	}()
+
+	err := execCmd(cmd, env)
+	require.NoError(err)
+	assert.Contains(bufout.String(), "FOO=BAR\n")
+	assert.Contains(bufout.String(), "ZOO=BAZ\n")
+	assert.Empty(buferr.String())
+}
+
+func Test_execCmd_WithArgs(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	cmd := []string{"/bin/sh", "-c", "echo $FOO $ZOO"}
+	env := map[string]string{
+		"FOO": "BAR",
+		"ZOO": "BAZ",
+	}
+
+	bufout := &bytes.Buffer{}
+	buferr := &bytes.Buffer{}
+	_stdout = bufout
+	_stderr = buferr
+
+	defer func() {
+		_stdout = os.Stdout
+		_stderr = os.Stderr
+	}()
+
+	err := execCmd(cmd, env)
+	require.NoError(err)
+	assert.Equal(bufout.String(), "BAR BAZ\n")
+	assert.Empty(buferr.String())
+}
+
+func Test_execCmd_Err(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	cmd := []string{"/bin/ls", "/not/exists"}
+	env := map[string]string{
+		"FOO": "BAR",
+		"ZOO": "BAZ",
+	}
+
+	bufout := &bytes.Buffer{}
+	buferr := &bytes.Buffer{}
+	_stdout = bufout
+	_stderr = buferr
+
+	defer func() {
+		_stdout = os.Stdout
+		_stderr = os.Stderr
+	}()
+
+	err := execCmd(cmd, env)
+	require.Error(err)
+	assert.Empty(bufout.String())
+	assert.NotEmpty(buferr.String())
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.30.1
 	github.com/aws/aws-sdk-go-v2/config v1.27.24
 	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.32.1
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
@@ -22,4 +23,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.30.1 // indirect
 	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
